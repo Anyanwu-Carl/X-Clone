c5b929c67c7a7d613db110755ae88da528a25f83
Anyanwu-Carl <anyanwuocee@gmail.com>
2026-04-05 21:16:17 +0100

    first commit

---
+++ lib/pages/signin_page.dart
@@ -0,0 +1,272 @@
+import 'package:flutter/material.dart';
+import 'package:tute_app/pages/signup_page.dart';
+
+class SignInPage extends StatefulWidget {
+  const SignInPage({super.key});
+
+  @override
+  State<SignInPage> createState() => _SignInPageState();
+}
+
+class _SignInPageState extends State<SignInPage> {
+  final GlobalKey<FormState> _signInKey = GlobalKey();
+  TextEditingController _emailController = TextEditingController();
+  TextEditingController _passwordController = TextEditingController();
+  final RegExp emailValid = RegExp(
+    r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+",
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Form(
+        key: _signInKey,
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.only(left: 20.0, right: 20.0, top: 120.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                // X logo
+                Image.asset("assets/images/x-logo.avif", height: 60),
+
+                const SizedBox(height: 30),
+
+                // Sign-in Text
+                Text(
+                  "Sign in to X",
+                  style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.grey.shade500,
+                  ),
+                ),
+
+                // LOG IN WITH GOOGLE BUTTON
+                Container(
+                  margin: const EdgeInsets.only(bottom: 25, top: 25),
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(30),
+                  ),
+                  child: TextButton(
+                    onPressed: () {
+                      if (_signInKey.currentState!.validate()) {
+                        debugPrint("Email: ${_emailController}");
+                        debugPrint("Password: ${_passwordController}");
+                      }
+                    },
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        // Google logo
+                        Image.asset("assets/images/google-img.png", height: 20),
+
+                        SizedBox(width: 10),
+
+                        // Sign-in with google text
+                        Text(
+                          "Sign in with Google",
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 18,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+
+                // LOG IN WITH APPLE BUTTON
+                Container(
+                  margin: const EdgeInsets.only(bottom: 25),
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(30),
+                  ),
+                  child: TextButton(
+                    onPressed: () {
+                      if (_signInKey.currentState!.validate()) {
+                        debugPrint("Email: ${_emailController}");
+                        debugPrint("Password: ${_passwordController}");
+                      }
+                    },
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        // Apple logo
+                        Image.asset("assets/images/apple-img.png", height: 20),
+
+                        SizedBox(width: 10),
+
+                        // Sign-in with apple text
+                        Text(
+                          "Sign in with Apple",
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 18,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+
+                // EMAIL
+                Container(
+                  margin: const EdgeInsets.only(bottom: 25),
+                  decoration: BoxDecoration(
+                    color: Colors.grey.shade900,
+                    borderRadius: BorderRadius.circular(30),
+                  ),
+                  child: TextFormField(
+                    controller: _emailController,
+                    keyboardType: TextInputType.emailAddress,
+                    decoration: InputDecoration(
+                      hintText: "Enter an email",
+                      hintStyle: TextStyle(color: Colors.grey.shade500),
+                      border: InputBorder.none,
+                      contentPadding: const EdgeInsets.symmetric(
+                        vertical: 15,
+                        horizontal: 20.0,
+                      ),
+                    ),
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return "Please enter an email";
+                      }
+                      // Email validation using regex
+                      else if (!emailValid.hasMatch(value)) {
+                        return "Please enter a valid email";
+                      }
+                      return null;
+                    },
+                  ),
+                ),
+
+                // PASSWORD
+                Container(
+                  margin: const EdgeInsets.only(bottom: 25),
+                  decoration: BoxDecoration(
+                    color: Colors.grey.shade900,
+                    borderRadius: BorderRadius.circular(30),
+                  ),
+                  child: TextFormField(
+                    controller: _passwordController,
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      hintText: "Enter a password",
+                      hintStyle: TextStyle(color: Colors.grey.shade500),
+                      border: InputBorder.none,
+                      contentPadding: const EdgeInsets.symmetric(
+                        vertical: 15,
+                        horizontal: 20.0,
+                      ),
+                    ),
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return "Please enter a password";
+                      } else if (value.length < 6) {
+                        return "Password must be at least 6 characters";
+                      }
+                      return null;
+                    },
+                  ),
+                ),
+
+                // SUBMIT BUTTON
+                Container(
+                  margin: const EdgeInsets.only(bottom: 25),
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(30),
+                  ),
+                  child: TextButton(
+                    onPressed: () {
+                      if (_signInKey.currentState!.validate()) {
+                        debugPrint("Email: ${_emailController}");
+                        debugPrint("Password: ${_passwordController}");
+                      }
+                    },
+                    child: Text(
+                      "Sign in",
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 18,
+                      ),
+                    ),
+                  ),
+                ),
+
+                // FORGOT PASSWORD BUTTON
+                Container(
+                  margin: EdgeInsets.only(bottom: 25),
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    color: Colors.transparent,
+                    borderRadius: BorderRadius.circular(30),
+                    border: Border.all(color: Colors.white, width: 2.0),
+                  ),
+                  child: TextButton(
+                    onPressed: () {
+                      if (_signInKey.currentState!.validate()) {
+                        debugPrint("Email: ${_emailController}");
+                        debugPrint("Password: ${_passwordController}");
+                      }
+                    },
+                    child: Text(
+                      "Forgot Password?",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                        fontSize: 18,
+                      ),
+                    ),
+                  ),
+                ),
+
+                // Don't have an account? Sign up
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    // Don't have an account? text
+                    Text(
+                      "Don't have an account?",
+                      style: TextStyle(color: Colors.grey.shade500),
+                    ),
+
+                    // Sign up button
+                    TextButton(
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => const SignUpPage(),
+                          ),
+                        );
+                      },
+                      child: const Text(
+                        "Sign Up!",
+                        style: TextStyle(
+                          color: Colors.blue,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/pages/signup_page.dart
@@ -0,0 +1,236 @@
+import 'package:flutter/material.dart';
+
+class SignUpPage extends StatefulWidget {
+  const SignUpPage({super.key});
+
+  @override
+  State<SignUpPage> createState() => _SignUpPageState();
+}
+
+class _SignUpPageState extends State<SignUpPage> {
+  final GlobalKey<FormState> _signUpKey = GlobalKey();
+  TextEditingController _emailController = TextEditingController();
+  TextEditingController _passwordController = TextEditingController();
+  final RegExp emailValid = RegExp(
+    r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+",
+  );
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Form(
+        key: _signUpKey,
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              // X logo
+              Image.asset("assets/images/x-logo.avif", height: 60),
+
+              const SizedBox(height: 30),
+
+              // Sign-up Text
+              Text(
+                "Sign up for X",
+                style: TextStyle(
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.grey.shade500,
+                ),
+              ),
+
+              // SIGN UP WITH GOOGLE BUTTON
+              Container(
+                margin: const EdgeInsets.only(bottom: 25, top: 25),
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(30),
+                ),
+                child: TextButton(
+                  onPressed: () {
+                    if (_signUpKey.currentState!.validate()) {
+                      debugPrint("Email: ${_emailController}");
+                      debugPrint("Password: ${_passwordController}");
+                    }
+                  },
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      // Google logo
+                      Image.asset("assets/images/google-img.png", height: 20),
+
+                      SizedBox(width: 10),
+
+                      // Sign-up with google text
+                      Text(
+                        "Sign up with Google",
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+
+              // SIGN UP WITH APPLE BUTTON
+              Container(
+                margin: const EdgeInsets.only(bottom: 25),
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(30),
+                ),
+                child: TextButton(
+                  onPressed: () {
+                    if (_signUpKey.currentState!.validate()) {
+                      debugPrint("Email: ${_emailController}");
+                      debugPrint("Password: ${_passwordController}");
+                    }
+                  },
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      // Apple logo
+                      Image.asset("assets/images/apple-img.png", height: 20),
+
+                      SizedBox(width: 10),
+
+                      // Sign-up with apple text
+                      Text(
+                        "Sign up with Apple",
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+
+              // EMAIL
+              Container(
+                margin: const EdgeInsets.only(bottom: 25),
+                decoration: BoxDecoration(
+                  color: Colors.grey.shade900,
+                  borderRadius: BorderRadius.circular(30),
+                ),
+                child: TextFormField(
+                  controller: _emailController,
+                  keyboardType: TextInputType.emailAddress,
+                  decoration: InputDecoration(
+                    hintText: "Enter an email",
+                    hintStyle: TextStyle(color: Colors.grey.shade500),
+                    border: InputBorder.none,
+                    contentPadding: const EdgeInsets.symmetric(
+                      vertical: 15,
+                      horizontal: 20.0,
+                    ),
+                  ),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return "Please enter an email";
+                    }
+                    // Email validation using regex
+                    else if (!emailValid.hasMatch(value)) {
+                      return "Please enter a valid email";
+                    }
+                    return null;
+                  },
+                ),
+              ),
+
+              // PASSWORD
+              Container(
+                margin: const EdgeInsets.only(bottom: 25),
+                decoration: BoxDecoration(
+                  color: Colors.grey.shade900,
+                  borderRadius: BorderRadius.circular(30),
+                ),
+                child: TextFormField(
+                  controller: _passwordController,
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    hintText: "Enter a password",
+                    hintStyle: TextStyle(color: Colors.grey.shade500),
+                    border: InputBorder.none,
+                    contentPadding: const EdgeInsets.symmetric(
+                      vertical: 15,
+                      horizontal: 20.0,
+                    ),
+                  ),
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return "Please enter a password";
+                    } else if (value.length < 6) {
+                      return "Password must be at least 6 characters";
+                    }
+                    return null;
+                  },
+                ),
+              ),
+
+              // SIGN UP BUTTON
+              Container(
+                margin: const EdgeInsets.only(bottom: 25),
+                width: double.infinity,
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(30),
+                ),
+                child: TextButton(
+                  onPressed: () {
+                    if (_signUpKey.currentState!.validate()) {
+                      debugPrint("Email: ${_emailController}");
+                      debugPrint("Password: ${_passwordController}");
+                    }
+                  },
+                  child: Text(
+                    "Sign Up",
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 18,
+                    ),
+                  ),
+                ),
+              ),
+
+              // Already have an account? Sign in
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  // Already have an account? text
+                  Text(
+                    "Already have an account?",
+                    style: TextStyle(color: Colors.grey.shade500),
+                  ),
+
+                  // Sign in button
+                  TextButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    child: const Text(
+                      "Sign In!",
+                      style: TextStyle(
+                        color: Colors.blue,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
