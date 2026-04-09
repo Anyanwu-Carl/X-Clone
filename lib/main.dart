ea87d4b2b444351c78210623aebdcc187a44ee75
Anyanwu-Carl <anyanwuocee@gmail.com>
2026-04-09 23:02:52 +0100

    fb

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:tute_app/firebase_options.dart';
 import 'package:tute_app/pages/signin_page.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
